92bc5d8e9a5bedde537dd081f3f0cabdf9168c75
Aman Bhattarai <41770152+invictusaman@users.noreply.github.com>
2026-02-18 19:35:54 -0500

    Create Alphabet_Rangoli.py

---
+++ Alphabet_Rangoli.py
@@ -0,0 +1,89 @@
+'''
+You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:
+
+#size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+
+# Sample Input
+5
+
+# Sample Output
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+Link to question : https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+'''
+
+def print_rangoli(size):
+    # your code goes here
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    output = []
+    width = 4 * size - 3
+    
+    for i in range(size):
+        str1 = '-'.join(alphabets[i:size])
+        row = (str1[::-1] + str1[1:]).center(width, '-')
+        output.append(row)
+    
+    # Print top half 
+    for line in output[::-1]:
+        print(line)
+
+    # Print bottom half (excluding middle)
+    for line in output[1:]:
+        print(line)
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
